1d2edd432e8dbf89fe1dec747d282fbcb331cb43
Jackson Blankenship <jacksblan@gmail.com>
2023-02-08 21:09:47 -0600

    add global flag to homebrew command to force Brewfile to live in home dir

--- .config/fish/functions/brew.fish
+++ .config/fish/functions/brew.fish
@@ -1,6 +1,6 @@
 # homebrew wrapper that keeps the brewfile up-to-date
 function brew -w brew
   if command brew $argv
-    command brew bundle dump --force
+    command brew bundle dump --force --global
   end
 end
