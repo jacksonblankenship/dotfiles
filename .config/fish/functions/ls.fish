a9b5dbb88fbe8149ef962841d367bb6aa5711bee
Jackson Blankenship <jacksblan@gmail.com>
2023-02-09 12:03:03 -0600

    Clean up fish functions (#7)
    * improve update script and add command prefix when available
    
    * add command prefix to exa
    
    * remove standup from git function
    
    * remove spare character at end of file in dotfiles.fish
    
    * change 'dotfiles' function to 'dot' since it's easier to type
    
    * add command prefix to ccat

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,7 +1,7 @@
 # replace boring ls with fun ls
 function ls -w ls
   if type -f exa &> /dev/null
-    exa --group-directories-first --icons --git $argv
+    command exa --group-directories-first --icons --git $argv
   else
     command ls $argv
   end
--- .config/fish/functions/update.fish
+++ .config/fish/functions/update.fish
@@ -1,15 +1,39 @@
 # function to update all packages
-function update --description "Update all relevant dependencies"
-  # update asdf core
-  asdf update \
-    # update asdf plugins
-    && asdf plugin update --all
-    # update package definitions and homebrew itself
-    && brew update \
-    # update all installed packages
-    && brew upgrade \
-    # update all fisher and all installed plugins
-    && fisher update \
-    # update all neovim plugins
-    && nvim +PlugUpdate +qa
+function update --description "Update all relevant dependencies" -w update
+  echo (set_color green) "[ ✅ ] Updating asdf core" (set_color normal)
+  if ! command asdf update
+    echo (set_color red) "[ ❌ ] Failed to update asdf core" (set_color normal)
+    return 1
+  end
+
+  echo (set_color green) "[ ✅ ] Updating asdf plugins" (set_color normal)
+  if ! command asdf plugin update --all
+    echo (set_color red) "[ ❌ ] Failed to update asdf plugins" (set_color normal)
+    return 1
+  end
+
+  echo (set_color green) "[ ✅ ] Updating homebrew and package definitions"  (set_color normal)
+  if ! command brew update
+    echo (set_color red) "[ ❌ ] Failed to update homebrew and package definitions" (set_color normal)
+    return 1
+  end
+
+  echo (set_color green) "[ ✅ ] Updating installed homebrew packages" (set_color normal)
+  if ! command brew upgrade
+    echo (set_color red) "[ ❌ ] Failed to update installed homebrew packages" (set_color normal)
+    return 1
+  end
+
+  echo (set_color green) "[ ✅ ] Updating fisher and all fisher plugins" (set_color normal)
+  if ! fisher update
+    echo (set_color red) "[ ❌ ] Failed to update fisher and all fisher plugins" (set_color normal)
+    return 1
+  end
+
+  echo (set_color green) "[ ✅ ] Updating neovim plugins" (set_color normal)
+  if ! command nvim +PlugUpdate +qa
+    echo (set_color red) "[ ❌ ] Failed to update neovim plugins" (set_color normal)
+    return 1
+  end
 end
+
