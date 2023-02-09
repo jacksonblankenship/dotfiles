4a61e361cbd6171221b59f5354cd865982ee21fa
Jackson Blankenship <jacksblan@gmail.com>
2023-02-09 12:13:42 -0600

    add dot function to version control

---
+++ .config/fish/functions/dot.fish
@@ -0,0 +1,10 @@
+function dot -w dot
+  if [ $argv[1] = 'add' ]; or [ $argv[1] = 'stage' ]
+    if [ $argv[2] = '.' ]; or [ $argv[2] = '-A' ]; or [ $argv[2] = '--all' ]
+      printf "'dot %s %s' is not allowed. Please add dotfiles individually or use '-u' instead.\n" $argv[1] $argv[2]
+      return 1
+    end
+  end
+
+  git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
+end
