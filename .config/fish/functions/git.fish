f8d1da02cf592e4cfa65835006c0a62a40d03953
Jackson Blankenship <jacksblan@gmail.com>
2023-05-16 10:24:55 -0500

    remove standup function from git, add 'fresh' function to git

--- .config/fish/functions/git.fish
+++ .config/fish/functions/git.fish
@@ -9,26 +9,12 @@ function git --wraps git
     # revert a repo out of a "work in progress" state
     case unwip
       command git log -n 1 | command grep -q -c "\-\-wip\-\-" && command git reset HEAD~1
-    case standup
-      # loop over all dates with committs since the most recent Sunday
-      command git log --reflog --format="%cd" --date="short" --since="last month" --author=(command git config --global user.name) \
-        # sort and remove duplicate dates
-        | sort -u \
-        # loop over the dates
-        | while read DATE;
-          # print out a pretty date header
-          echo -e "\n\x1B[34m[ $DATE ]\x1B[0m"
-          # get all of the committs for the particular day
-          command git log --reflog --format="%s" --author=(command git config --global user.name) --since="$DATE 00:00:00" --until="$DATE 23:59:59" \
-            # remove any gwip commits
-            | sed 's/\--wip-- \[skip ci\]//g' \
-            # remove any blank lines left behind by sed
-            | awk 'NF' \
-            # sort and remove duplicate commit messages
-            | sort -u \
-            # make the committs a bulletted list
-            | sed 's/^/ • /'
-        end
+    case fresh
+      git checkout main
+      git fetch --all -Pp
+      git rebase origin/main
+      git branch | grep -v "main" | xargs git branch -D
+      git status
     # execute git normally
     case '*'
       command git $argv
