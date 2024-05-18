865d499dbaab93bf4975a5c2d778208fed723c83
Jackson Blankenship <jacksblan@gmail.com>
2024-05-17 22:22:30 -0500

    correct issue with auto-brewfile

--- .config/fish/functions/brew.fish
+++ .config/fish/functions/brew.fish
@@ -1,7 +1,8 @@
-# homebrew wrapper that keeps the ~/.Brewfile up-to-date
+# Homebrew wrapper that keeps the ~/.Brewfile up-to-date
 function brew --wraps brew
   if command brew $argv
     and command brew bundle dump --force --global
-      and sed -i '' '/^vscode\|^cask "font-\|^tap "homebrew\/cask-fonts"/d' ~/.Brewfile
+      and grep -vE '^vscode|^cask "font-|^tap "homebrew/cask-fonts"' ~/.Brewfile > ~/.Brewfile.tmp
+      and mv ~/.Brewfile.tmp ~/.Brewfile
   end
 end
