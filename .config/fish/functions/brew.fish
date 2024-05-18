5b3c7885f216930e1eef740371e22755ae237aa8
Jackson Blankenship <jacksblan@gmail.com>
2024-05-17 21:38:24 -0500

    fix brewfile vscode issue

--- .config/fish/functions/brew.fish
+++ .config/fish/functions/brew.fish
@@ -1,6 +1,6 @@
 # homebrew wrapper that keeps the brewfile up-to-date
 function brew --wraps brew
   if command brew $argv
-    command brew bundle dump --force --global
+    command brew bundle dump --force --global && sed -i '' '/^vscode/d' ~/.Brewfile
   end
 end
